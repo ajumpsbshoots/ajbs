4dddc087fffab7cd0aa4cfd2e90042e119769bc8
Matthew Hunter <matthewhunter@Matthews-MacBook-Pro.local>
2013-11-19 19:42:30 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Albs::Application.config.secret_key_base = '6f4be1f144680ef43af892364b1f571ca4859cf132fd496c5e050e85da68626e018a4ce30d4f990da31b3dea48d3cdc8357fc8b33d403cff892d37fc19e7c166'
